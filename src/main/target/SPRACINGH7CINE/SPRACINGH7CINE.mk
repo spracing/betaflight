a98f51e300f2eeab0df823d2962096ad1a1d8809
Dominic Clifton <dominic.clifton@cleanflight.com>
2019-07-10 13:17:30 +0200

    SPRACINGH7CINE - Basic target support.

---
+++ src/main/target/SPRACINGH7CINE/SPRACINGH7CINE.mk
@@ -0,0 +1,20 @@
+FEATURES       += VCP ONBOARDFLASH SDCARD_SDIO
+
+ifneq ($(EXST),)
+EXST = yes
+endif
+
+ifneq ($(EXST),yes)
+TARGET_FLASH_SIZE := 1024
+LD_SCRIPT       = $(LINKER_DIR)/stm32_flash_h750_1m.ld
+endif
+
+TARGET_SRC += \
+            drivers/accgyro/accgyro_mpu.c \
+            drivers/accgyro/accgyro_mpu6500.c \
+            drivers/accgyro/accgyro_spi_mpu6500.c \
+            drivers/accgyro/accgyro_spi_icm42605.c \
+            drivers/compass/compass_hmc5883l.c \
+            drivers/barometer/barometer_bmp388.c \
+            drivers/vtx_rtc6705_soft_spi.c
+
---
+++ src/main/target/SPRACINGH7CINE/target.mk
@@ -0,0 +1,8 @@
+H750xB_TARGETS += $(TARGET)
+
+HSE_VALUE    = 8000000
+
+TARGET_SRC += \
+            drivers/bus_quadspi_hal.c \
+            drivers/bus_quadspi.c \
+
