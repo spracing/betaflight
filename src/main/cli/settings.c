b6133f4256c832a157c3446d708405ec9ddbc78a
Dominic Clifton <dominic.clifton@cleanflight.com>
2020-02-24 21:39:27 +0100

    Fix vcdProfile not being exposed in CLI for non-max7456 OSDs.
    * The FRSky OSD already used vcdProfile in init.c.

--- src/main/cli/settings.c
+++ src/main/cli/settings.c
@@ -372,11 +372,11 @@ const char * const lookupTableRescueAltitudeMode[] = {
 };
 #endif
 
-#ifdef USE_MAX7456
+#if defined(USE_MAX7456) || defined(USE_FRSKYOSD) || defined(USE_SPRACING_PIXEL_OSD)
 static const char * const lookupTableVideoSystem[] = {
     "AUTO", "PAL", "NTSC"
 };
-#endif // USE_MAX7456
+#endif
 
 #if defined(USE_ITERM_RELAX)
 const char * const lookupTableItermRelax[] = {
@@ -566,9 +566,9 @@ const lookupTableEntry_t lookupTables[] = {
     LOOKUP_TABLE_ENTRY(lookupTableGyro),
 #endif
     LOOKUP_TABLE_ENTRY(lookupTableThrottleLimitType),
-#ifdef USE_MAX7456
+#if defined(USE_MAX7456) || defined(USE_FRSKYOSD) || defined(USE_SPRACING_PIXEL_OSD)
     LOOKUP_TABLE_ENTRY(lookupTableVideoSystem),
-#endif // USE_MAX7456
+#endif
 #if defined(USE_ITERM_RELAX)
     LOOKUP_TABLE_ENTRY(lookupTableItermRelax),
     LOOKUP_TABLE_ENTRY(lookupTableItermRelaxType),
@@ -1423,8 +1423,7 @@ const clivalue_t valueTable[] = {
     { "vtx_spi_bus",                VAR_UINT8  | HARDWARE_VALUE | MASTER_VALUE, .config.minmaxUnsigned = { 0, SPIDEV_COUNT }, PG_VTX_IO_CONFIG, offsetof(vtxIOConfig_t, spiDevice) },
 #endif
 
-// PG_VCD_CONFIG
-#ifdef USE_MAX7456
+#if defined(USE_MAX7456) || defined(USE_FRSKYOSD) || defined(USE_SPRACING_PIXEL_OSD)
     { "vcd_video_system",           VAR_UINT8   | MASTER_VALUE | MODE_LOOKUP, .config.lookup = { TABLE_VIDEO_SYSTEM }, PG_VCD_CONFIG, offsetof(vcdProfile_t, video_system) },
     { "vcd_h_offset",               VAR_INT8    | MASTER_VALUE, .config.minmax = { -32, 31 }, PG_VCD_CONFIG, offsetof(vcdProfile_t, h_offset) },
     { "vcd_v_offset",               VAR_INT8    | MASTER_VALUE, .config.minmax = { -15, 16 }, PG_VCD_CONFIG, offsetof(vcdProfile_t, v_offset) },
