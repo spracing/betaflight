32bc832628951b257fcf688db7593aadd41c64a3
Dominic Clifton <dominic.clifton@cleanflight.com>
2020-03-26 17:28:02 +0100

    SPRacingH7NP - Initial target support.

---
+++ src/main/target/SPRACINGH7NP/target.mk
@@ -0,0 +1,49 @@
+RX_SRC = \
+    rx/cc2500_common.c \
+    rx/cc2500_frsky_shared.c \
+    rx/cc2500_frsky_d.c \
+    rx/cc2500_frsky_x.c \
+    rx/cc2500_sfhss.c \
+    rx/cc2500_redpine.c \
+    rx/a7105_flysky.c \
+    rx/cyrf6936_spektrum.c \
+    drivers/rx/expresslrs_driver.c \
+    rx/expresslrs.c \
+    rx/expresslrs_common.c \
+    rx/expresslrs_telemetry.c \
+    drivers/rx/rx_cc2500.c \
+    drivers/rx/rx_a7105.c \
+    drivers/rx/rx_cyrf6936.c \
+    drivers/rx/rx_sx127x.c \
+    drivers/rx/rx_sx1280.c \
+
+H750xB_TARGETS += $(TARGET)
+
+HSE_VALUE    = 8000000
+
+FEATURES       += VCP ONBOARDFLASH SDCARD_SDIO SPRACING_PIXEL_OSD
+
+EXST ?= yes
+EXST_ADJUST_VMA = 0x97CE0000
+
+ifneq ($(EXST),)
+EXST = yes
+LD_SCRIPT       = $(LINKER_DIR)/stm32_ram_h750_exst_spracingpixelosd.ld
+endif
+
+ifneq ($(EXST),yes)
+TARGET_FLASH_SIZE := 1024
+LD_SCRIPT       = $(LINKER_DIR)/stm32_flash_h750_1m_spracingpixelosd.ld
+endif
+
+TARGET_SRC += \
+            drivers/bus_quadspi_hal.c \
+            drivers/bus_quadspi.c \
+            $(addprefix drivers/accgyro/,$(notdir $(wildcard $(SRC_DIR)/drivers/accgyro/*.c))) \
+            $(ROOT)/lib/main/BoschSensortec/BMI270-Sensor-API/bmi270_maximum_fifo.c \
+            $(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
+            $(addprefix drivers/barometer/,$(notdir $(wildcard $(SRC_DIR)/drivers/barometer/*.c))) \
+	        drivers/vtx_rtc6705.c \
+	        drivers/vtx_rtc6705_soft_spi.c \
+	        $(RX_SRC)
+            
