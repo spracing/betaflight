c3a3901b87a194c8b9208ade12e8efd034fbda20
Dominic Clifton <dominic.clifton@cleanflight.com>
2020-06-11 21:51:23 +0200

    Allow specification of EXST flash chip.
    e.g.
    
    ```
    make CONFIG=SPRACINGH7EXTREME EXST_FLASH_CHIP=W25N01GV
    make CONFIG=SPRACINGH7EXTREME EXST_FLASH_CHIP=W25Q128JV
    ```
    
    If flash chip is not specified, the target configured default one is
    used.
    
    Flash chip and load address is added to the build file.
    
    e.g.
    
    ```
    ./obj/betaflight_4.2.0_SPRACINGH7EXTREME-W25N01GV_<revision>.bin
    ./obj/betaflight_4.2.0_SPRACINGH7EXTREME-W25Q128JV_<norevision>.bin
    ```

--- src/main/target/STM32H750/target.mk
+++ src/main/target/STM32H750/target.mk
@@ -7,3 +7,24 @@ ifneq ($(EXST),yes)
 TARGET_FLASH_SIZE   := 1024
 LD_SCRIPT            = $(LINKER_DIR)/stm32_flash_h750_1m.ld
 endif
+
+ifeq ($(EXST),yes)
+# EXST flash chip configuration
+
+ifeq ($(EXST_FLASH_CHIP),)
+EXST_FLASH_CHIP = W25N01GV
+endif
+
+ifeq ($(EXST_FLASH_CHIP),W25N01GV)
+EXST_ADJUST_VMA = 0x97CE0000 # 128MB NAND flash
+else
+ifeq ($(EXST_FLASH_CHIP),W25Q128JV)
+EXST_ADJUST_VMA = 0x90F90000 # 16MB NOR flash
+endif
+endif
+
+#ifeq ($(EXST_ADJUST_VMA),)
+#EXST_ADJUST_VMA = 0x97CE0000
+#endif
+
+endif
