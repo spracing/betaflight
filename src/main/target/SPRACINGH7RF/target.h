5bf1b6dd3d16673f82a3e310af6bd6ab0f4e4fdc
Dominic Clifton <me@dominicclifton.name>
2021-04-15 04:23:24 +0200

    SPRacingH7RF - IO/Timer updates.

--- src/main/target/SPRACINGH7RF/target.h
+++ src/main/target/SPRACINGH7RF/target.h
@@ -269,7 +269,10 @@
 #define TARGET_IO_PORTE 0xffff
 #define TARGET_IO_PORTF 0xffff
 #define TARGET_IO_PORTG 0xffff
+#define TARGET_IO_PORTH 0xffff
+#define TARGET_IO_PORTI 0xffff
 
-#define USABLE_TIMER_CHANNEL_COUNT 19
+#define USABLE_TIMER_CHANNEL_COUNT 13
 
-#define USED_TIMERS  ( TIM_N(1) | TIM_N(2) | TIM_N(3) | TIM_N(4) | TIM_N(5) | TIM_N(8) | TIM_N(12) | TIM_N(15) )
+// TIM1, TIM2, TIM15 reserved for OSD
+#define USED_TIMERS  ( TIM_N(3) | TIM_N(4) | TIM_N(5) | TIM_N(8) | TIM_N(17) )
