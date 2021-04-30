56253c7309adb6e435d4d51c1486d192e38f6f28
Dominic Clifton <me@dominicclifton.name>
2021-04-30 13:17:15 +0200

    STM32H730 - Use 50Mhz clock for SDXC cards.
    * Tested with SanDisk Ultra 64GB.  100Mhz clock gave CRC errors.

--- src/main/drivers/sdio_h7xx.c
+++ src/main/drivers/sdio_h7xx.c
@@ -276,7 +276,8 @@ SD_Error_t SD_Init(void)
         hsd1.Init.BusWide = SDMMC_BUS_WIDE_1B; // FIXME untested
     }
     hsd1.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_ENABLE;
-    hsd1.Init.ClockDiv = 1; // 200Mhz / (2 * 1 ) = 100Mhz, used for "UltraHigh speed SD card" only, see   HAL_SD_ConfigWideBusOperation, SDMMC_HSpeed_CLK_DIV, SDMMC_NSpeed_CLK_DIV
+//    hsd1.Init.ClockDiv = 1; // 200Mhz / (2 * 1 ) = 100Mhz, used for "UltraHigh speed SD card" only, see   HAL_SD_ConfigWideBusOperation, SDMMC_HSpeed_CLK_DIV, SDMMC_NSpeed_CLK_DIV
+    hsd1.Init.ClockDiv = 2; // 200Mhz / (2 * 2 ) = 50Mhz, used for "UltraHigh speed SD card" only, see   HAL_SD_ConfigWideBusOperation, SDMMC_HSpeed_CLK_DIV, SDMMC_NSpeed_CLK_DIV
 
     status = HAL_SD_Init(&hsd1); // Will call HAL_SD_MspInit
 
