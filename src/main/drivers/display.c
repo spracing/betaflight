98e2d9aa4b13e0b2282552c954c10c681db354c0
Dominic Clifton <dominic.clifton@cleanflight.com>
2021-05-16 21:41:53 +0200

    OSD - Ensure that the displayClear during OSD init is called within a transaction.

--- src/main/drivers/display.c
+++ src/main/drivers/display.c
@@ -217,7 +217,11 @@ bool displaySupportsOsdSymbols(displayPort_t *instance)
 void displayInit(displayPort_t *instance, const displayPortVTable_t *vTable, displayPortDeviceType_e deviceType)
 {
     instance->vTable = vTable;
+
+    instance->vTable->beginTransaction(instance, DISPLAY_TRANSACTION_OPT_NONE);
     instance->vTable->clearScreen(instance);
+    instance->vTable->commitTransaction(instance);
+
     instance->useFullscreen = false;
     instance->cleared = true;
     instance->grabCount = 0;
