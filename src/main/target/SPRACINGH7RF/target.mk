3e6e1e8e06e6e965cfeaabca3179705eb390905f
Dominic Clifton <dominic.clifton@cleanflight.com>
2021-05-19 11:40:34 +0200

    SPRacingH7RF - Add ExpressLRS support. Fix SPI2 CLK pin.

--- src/main/target/SPRACINGH7RF/target.mk
+++ src/main/target/SPRACINGH7RF/target.mk
@@ -35,3 +35,7 @@ TARGET_SRC += \
             $(addprefix drivers/accgyro/,$(notdir $(wildcard $(SRC_DIR)/drivers/accgyro/*.c))) \
             $(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
             $(addprefix drivers/barometer/,$(notdir $(wildcard $(SRC_DIR)/drivers/barometer/*.c))) \
+            drivers/rx/rx_sx1280.c \
+            drivers/rx/rx_sx127x.c \
+            rx/expresslrs_common.c \
+            rx/expresslrs.c \
