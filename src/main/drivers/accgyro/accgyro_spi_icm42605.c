0ac506d0567c84165579ceb568ad2d8258ef7aef
Dominic Clifton <dominic.clifton@cleanflight.com>
2021-07-13 14:01:23 +0200

    ICM426xx - Avoid long startup delays when target allows 42605 and 42688P.
    If the target has a 42688P sensor there is a 20*150ms (3 second) delay
    caused by the 42605 driver's detect code.
    
    The 42605/42688P sensors are pin-compatible so it makes sense that a
    target would specify BOTH sensors to allow flexibility in manufacturing.

--- src/main/drivers/accgyro/accgyro_spi_icm42605.c
+++ src/main/drivers/accgyro/accgyro_spi_icm42605.c
@@ -126,6 +126,9 @@ uint8_t icm42605SpiDetect(const busDevice_t *bus)
         case ICM42605_WHO_AM_I_CONST:
             icmDetected = ICM_42605_SPI;
             break;
+        case ICM42688P_WHO_AM_I_CONST:
+            icmDetected = ICM_42688P_SPI;
+            break;
         default:
             icmDetected = MPU_NONE;
             break;
