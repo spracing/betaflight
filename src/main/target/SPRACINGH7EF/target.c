5f143a7f39cdf5a89ad6a22f66043d88fc6e964e
Dominic Clifton <dominic.clifton@cleanflight.com>
2021-07-30 19:32:47 +0200

    *** SPRacingH7EF - Fix timer/motor configuration

--- src/main/target/SPRACINGH7EF/target.c
+++ src/main/target/SPRACINGH7EF/target.c
@@ -38,10 +38,10 @@ const timerHardware_t timerHardware[USABLE_TIMER_CHANNEL_COUNT] = {
     DEF_TIM(TIM5,  CH4, PA3,  TIM_USE_MOTOR,               0,  3,  0 ), // M4
 
     // On 4in1ESC_2
-    DEF_TIM(TIM3,  CH1, PA6,  TIM_USE_NONE,                0,  4,  0 ), // M5
-    DEF_TIM(TIM3,  CH2, PA7,  TIM_USE_NONE,                0,  5,  0 ), // ..
-    DEF_TIM(TIM3,  CH3, PB0,  TIM_USE_NONE,                0,  6,  0 ), // ..
-    DEF_TIM(TIM3,  CH4, PB1,  TIM_USE_NONE,                0,  7,  0 ), // M8
+    DEF_TIM(TIM3,  CH1, PA6,  TIM_USE_MOTOR,               0,  4,  0 ), // M5
+    DEF_TIM(TIM3,  CH2, PA7,  TIM_USE_MOTOR,               0,  5,  0 ), // ..
+    DEF_TIM(TIM3,  CH3, PB0,  TIM_USE_MOTOR,               0,  6,  0 ), // ..
+    DEF_TIM(TIM3,  CH4, PB1,  TIM_USE_MOTOR,               0,  7,  0 ), // M8
 
     // On GPS_IO1
     DEF_TIM(TIM4,  CH1, PD12, TIM_USE_MOTOR,               0,  8,  1 ), // M9  / I2C4_SCL
