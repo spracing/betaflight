165e97654451a0dfb0d48c4870c45b69aa1e1163
Dominic Clifton <dominic.clifton@cleanflight.com>
2021-08-10 16:38:02 +0200

    STM32H730 - Only move IRQ handlers to FAST_CODE on H7.
    Other MCUs are too short on ITCM.

--- src/main/drivers/serial_uart_hal.c
+++ src/main/drivers/serial_uart_hal.c
@@ -271,7 +271,7 @@ void uartDmaIrqHandler(dmaChannelDescriptor_t* descriptor)
 }
 #endif
 
-FAST_CODE void uartIrqHandler(uartPort_t *s)
+HOT_IRQ_HANDLER void uartIrqHandler(uartPort_t *s)
 {
     UART_HandleTypeDef *huart = &s->Handle;
     /* UART in mode Receiver ---------------------------------------------------*/
