6a745beb33ecdb555b7f6fa32a22c156a94209af
Dominic Clifton <dominic.clifton@cleanflight.com>
2021-08-20 21:12:36 +0200

    ExpressLRS - Use a prescaler that can handle the frequency shift.

--- src/main/rx/expresslrs_hal.c
+++ src/main/rx/expresslrs_hal.c
@@ -40,6 +40,8 @@
 #include "rx/expresslrs_common.h"
 #include "rx/expresslrs_impl.h"
 
+#include "common/maths.h"
+
 #define TIMER_INTERVAL_US_DEFAULT 20000
 #define TICK_TOCK_COUNT 2
 
@@ -51,9 +53,56 @@ typedef enum {
 typedef struct elrsTimerState_s {
     volatile tickTock_e tickTock;
     uint32_t intervalUs;
+    int32_t frequencyOffsetTicks;
+
+    int32_t phaseShift;
+
 } elrsTimerState_t;
 
-static elrsTimerState_t timerState = { 0 };
+// Use a little ram to keep the amount of CPU cycles used in the ISR lower.
+typedef struct elrsPhaseShiftLimits_s {
+    int32_t min;
+    int32_t max;
+} elrsPhaseShiftLimits_t;
+
+elrsPhaseShiftLimits_t phaseShiftLimits;
+
+static elrsTimerState_t timerState = {
+    TOCK, // Start on TOCK (in ELRS isTick is initialised to false)
+    TIMER_INTERVAL_US_DEFAULT,
+    0,
+    0
+};
+
+static void expressLrsRecalculatePhaseShiftLimits(void)
+{
+    phaseShiftLimits.max = (timerState.intervalUs / TICK_TOCK_COUNT);
+    phaseShiftLimits.min = -phaseShiftLimits.max;
+}
+
+uint16_t expressLrsCalculateMaximumExpectedPeriod(uint16_t intervalUs)
+{
+    // The timer reload register must not overflow when frequencyOffsetTicks is added to it.
+    // frequencyOffsetTicks is not expected to be higher than 1/4 of the interval.
+    // also, timer resolution must be as high as possible.
+    const uint16_t maximumExpectedPeriod = (intervalUs / TICK_TOCK_COUNT) + (timerState.intervalUs / 4);
+    return maximumExpectedPeriod;
+}
+
+void expressLrsUpdateTimerInterval(TIM_TypeDef *timer, uint16_t intervalUs)
+{
+    timerState.intervalUs = intervalUs;
+    expressLrsRecalculatePhaseShiftLimits();
+
+    timerReconfigureTimeBase(timer, expressLrsCalculateMaximumExpectedPeriod(timerState.intervalUs), MHZ_TO_HZ(1));
+
+    LL_TIM_SetAutoReload(timer, ((intervalUs / TICK_TOCK_COUNT) - 1) & 0xffff);
+}
+
+void expressLrsUpdatePhaseShift(int32_t newPhaseShift)
+{
+    timerState.phaseShift = constrain(newPhaseShift, phaseShiftLimits.min, phaseShiftLimits.max);
+}
 
 static void expressLrsOnTimerUpdate(timerOvrHandlerRec_t *cbRec, captureCompare_t capture)
 {
@@ -80,13 +129,15 @@ static void expressLrsOnTimerUpdate(timerOvrHandlerRec_t *cbRec, captureCompare_
     UNUSED(self);
 }
 
+
 void expressLrsInitialiseTimer(elrsReceiver_t *receiver)
 {
     receiver->timer = RX_EXPRESSLRS_TIMER_INSTANCE;
 
-    timerState.intervalUs = TIMER_INTERVAL_US_DEFAULT;
 
-    configTimeBase(receiver->timer, timerState.intervalUs / TICK_TOCK_COUNT, MHZ_TO_HZ(1));
+    configTimeBase(receiver->timer, expressLrsCalculateMaximumExpectedPeriod(timerState.intervalUs), MHZ_TO_HZ(1));
+
+    expressLrsUpdateTimerInterval(receiver->timer, timerState.intervalUs);
 
     timerChOvrHandlerInit(&receiver->timerUpdateCb, expressLrsOnTimerUpdate);
 
