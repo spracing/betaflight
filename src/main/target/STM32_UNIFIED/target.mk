4f4148460ca6ebe08ece831701bca43ebd07578b
Dominic Clifton <dominic.clifton@cleanflight.com>
2021-08-25 17:12:43 +0200

    ExpressLRS - Update other targets so the hal implementation

--- src/main/target/STM32_UNIFIED/target.mk
+++ src/main/target/STM32_UNIFIED/target.mk
@@ -54,8 +54,9 @@ TARGET_SRC = \
     rx/cc2500_redpine.c \
     rx/a7105_flysky.c \
     rx/cyrf6936_spektrum.c \
-    rx/expresslrs_common.c \
     rx/expresslrs.c \
+    rx/expresslrs_common.c \
+    rx/expresslrs_hal.c \
     drivers/rx/rx_cc2500.c \
     drivers/rx/rx_a7105.c \
     drivers/rx/rx_cyrf6936.c \
