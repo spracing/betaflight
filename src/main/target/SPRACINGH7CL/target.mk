6c7cb2b8af405c9bdc5f77e4854f2bfdfa5369f3
Dominic Clifton <dominic.clifton@cleanflight.com>
2022-11-22 15:55:58 +0100

    SPRacingH7CL - Required changes for recent build-system changes.

--- src/main/target/SPRACINGH7CL/target.mk
+++ src/main/target/SPRACINGH7CL/target.mk
@@ -19,19 +19,23 @@ HSE_VALUE    = 8000000
 # * OSD fonts
 # * OSD logos
 
+EXST ?= yes
+
 ifneq ($(EXST),)
 EXST = yes
 EXST_ADJUST_VMA = 0x90100000
+LD_SCRIPT       = $(LINKER_DIR)/stm32_ram_h730_exst_spracingpixelosd.ld
 endif
 
-FEATURES       += VCP ONBOARDFLASH
+FEATURES       += VCP ONBOARDFLASH SPRACING_PIXEL_OSD
 
 TARGET_SRC += \
             drivers/bus_quadspi_hal.c \
             drivers/bus_quadspi.c \
             drivers/accgyro/accgyro_mpu.c \
             drivers/accgyro/accgyro_mpu6500.c \
+            drivers/accgyro/accgyro_spi_mpu6500.c \
             drivers/accgyro/accgyro_spi_icm426xx.c \
-            drivers/compass/compass_hmc5883l.c \
-            drivers/compass/compass_qmc5883l.c \
-            drivers/barometer/barometer_bmp388.c \
+            $(addprefix drivers/compass/,$(notdir $(wildcard $(SRC_DIR)/drivers/compass/*.c))) \
+            $(addprefix drivers/barometer/,$(notdir $(wildcard $(SRC_DIR)/drivers/barometer/*.c))) \
+            
